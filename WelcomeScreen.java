befd6852c4e439ec8d9a90ecf4e2033b0b9045b4
Memduh Tutuş <79745254+memduhtutus@users.noreply.github.com>
2022-04-30 14:12:25 +0300

    Update WelcomeScreen.java

--- WelcomeScreen.java
+++ WelcomeScreen.java
@@ -2,22 +2,39 @@ package com.memduhtutus.bilgym;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class WelcomeScreen extends AppCompatActivity {
+    FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_screen);
+        setContentView(R.layout.activity_main);
+      //  FirebaseUser user = auth.getCurrentUser();
+        /*if(user != null) {
+            Intent intent = new Intent(WelcomeScreen.this, LogInScreen.class);
+            startActivity(intent);
+            finish();
 
+        }*/
     }
-    public void signUpClicked(View view){
 
+    public void signUpClicked(View view){
+        Intent intent = new Intent(WelcomeScreen.this, SignUpScreen.class);
+        startActivity(intent);
+        finish();
 
     }
     public void logInClicked(View view){
+        Intent intent = new Intent(WelcomeScreen.this, LogInScreen.class);
+        startActivity(intent);
+        finish();
 
     }
 }
